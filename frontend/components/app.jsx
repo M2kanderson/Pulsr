45f36038e9df6c1473be6299ff0effe2f42fb4cc
Justin Anderson <janderson2k@gmail.com>
2016-06-26 23:06:56 -0700

    Created Frontend

---
+++ frontend/components/app.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+const App = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+
+});
+
+module.exports = App;
---
+++ frontend/pulsr.jsx
@@ -0,0 +1,24 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const ReactRouter = require('react-router');
+const Router = ReactRouter.Router;
+const Route = ReactRouter.Route;
+const IndexRoute = ReactRouter.IndexRoute;
+const hashHistory = ReactRouter.hashHistory;
+
+const App = require('./components/app.jsx');
+
+const appRouter = (
+  <Router history={ hashHistory }>
+    <Route path="/" component={ App }>
+
+    </Route>
+  </Router>
+);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const root = document.getElementById('content');
+  ReactDOM.render(appRouter, root);
+});
